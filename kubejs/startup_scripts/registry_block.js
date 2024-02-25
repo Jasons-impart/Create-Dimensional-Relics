f2b8e4f2e92b21de8443294517b9b14f10f1a96f
CPearl0 <1269596929@qq.com>
2024-02-25 17:09:16 +0800

    Localizating...

--- kubejs/startup_scripts/registry_block.js
+++ kubejs/startup_scripts/registry_block.js
@@ -1,5 +1,5 @@
-StartupEvents.registry('block',e=>{
-    let Block = (
+StartupEvents.registry('block', e => {
+    function registerBlock(
         Name,
         DisplayName,
         SoundType,
@@ -7,8 +7,8 @@ StartupEvents.registry('block',e=>{
         Resistance,
         Tool,
         Grade,
-        RequiresTool        
-    ) =>{
+        RequiresTool
+    ) {
         e.create(Name)
             .displayName(DisplayName)
             .soundType(SoundType)
@@ -18,14 +18,15 @@ StartupEvents.registry('block',e=>{
             .tagBlock(Grade)
             .requiresTool(RequiresTool)
     }
-    //暗淡太空合金块
-    Block('bleak_space_block', '暗淡太空合金块', 'metal', 15, 120, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
-    //太空合金块
-    Block('space_block', '太空合金块', 'metal', 15, 1200, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
-    //奇迹块
-    Block('miracle_block', '奇迹块', 'amethyst', 10, 10, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
-    //深渊合金块
-    Block('abyss_block', '深渊合金块', 'metal', 15, 1200, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
-    //模型测试物品
-    Block('model_test', '模型测试', 'metal', 15, 15, 'mineable/pickaxe', 'minecraft:need_stone_tool', false)
+    
+    // 暗淡太空合金块
+    registerBlock('bleak_space_block', '暗淡太空合金块', 'metal', 15, 120, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
+    // 太空合金块
+    registerBlock('space_block', '太空合金块', 'metal', 15, 1200, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
+    // 奇迹块
+    registerBlock('miracle_block', '奇迹块', 'amethyst', 10, 10, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
+    // 深渊合金块
+    registerBlock('abyss_block', '深渊合金块', 'metal', 15, 1200, 'mineable/pickaxe', 'forge:needs_netherite_tool', true)
+    // 模型测试物品
+    registerBlock('model_test', '模型测试', 'metal', 15, 15, 'mineable/pickaxe', 'minecraft:need_stone_tool', false)
 })
--- kubejs/startup_scripts/registry_tiers.js
+++ kubejs/startup_scripts/registry_tiers.js
@@ -1,6 +1,6 @@
 //注册azii的眼罩
-ItemEvents.armorTierRegistry(e=>{
-    e.add("aziis_eye_patch", tier=>{
+ItemEvents.armorTierRegistry(e => {
+    e.add("aziis_eye_patch", tier => {
         tier.durabilityMultiplier = 10000
         tier.slotProtections = [0, 0, 0, 0]
         tier.toughness = 0
@@ -10,43 +10,65 @@ ItemEvents.armorTierRegistry(e=>{
         tier.equipSound = 'minecraft:item.armor.equip_leather'
     })
 })
-StartupEvents.registry('item', e=>{
-    e.create("aziis_eye_patch", 'helmet').displayName('老馋二服主的眼罩').tier("aziis_eye_patch").rarity('uncommon')
+StartupEvents.registry('item', e => {
+    e.create("aziis_eye_patch", 'helmet')
+        .displayName('老馋二服主的眼罩')
+        .tier("aziis_eye_patch")
+        .rarity('uncommon')
 })
 //注册太空合金套装及工具等级
-ItemEvents.toolTierRegistry(e=>{
-    e.add('space_alloyed',tier=>{
+ItemEvents.toolTierRegistry(e => {
+    e.add('space_alloy', tier => {
         tier.uses = 6000
         tier.attackDamageBonus = 16
         tier.level = 4
         tier.enchantmentValue = 0
-        tier.repairIngredient = '#kjs:bleak_space_ingot'
+        tier.repairIngredient = '#kjs:bleak_space_alloy_ingot'
         tier.speed = 25
     })
 })
-ItemEvents.armorTierRegistry(e=>{
-    e.add('space_alloyed',tier=>{
+ItemEvents.armorTierRegistry(e => {
+    e.add('space_alloy', tier => {
         tier.durabilityMultiplier = 300
-        tier.slotProtections = [10,15,13,10]
+        tier.slotProtections = [10, 15, 13, 10]
         tier.toughness = 10
         tier.knockbackResistance = 0.22
         tier.enchantmentValue = 0
-        tier.repairIngredient = '#kjs:bleak_space_ingot'
+        tier.repairIngredient = '#kjs:bleak_space_alloy_ingot'
         tier.equipSound = 'minecraft:item.armor.equip_diamond'
     })
 })
 //注册太空合金系列物品
-StartupEvents.registry('item', e=>{
-    e.create('space_alloyed_helmet','helmet').displayName('太空合金头盔').tier('space_alloyed').rarity('epic')
-    e.create('space_alloyed_boots','boots').displayName('太空合金靴子').tier('space_alloyed').rarity('epic')
-    e.create('space_alloyed_leggings','leggings').displayName('太空合金护腿').tier('space_alloyed').rarity('epic')
-    e.create('space_alloyed_chestplate','chestplate').displayName('太空合金胸甲').tier('space_alloyed').rarity('epic')
-    e.create('space_alloyed_pickaxe','pickaxe').displayName('太空合金稿').rarity('epic').tier('space_alloyed')
-    e.create('space_alloyed_sword','sword').displayName('太空合金剑').rarity('epic').tier('space_alloyed').speedBaseline(-2.0)
+StartupEvents.registry('item', e => {
+    e.create('space_alloy_helmet', 'helmet')
+        .displayName('太空合金头盔')
+        .tier('space_alloy')
+        .rarity('epic')
+    e.create('space_alloy_boots', 'boots')
+        .displayName('太空合金靴子')
+        .tier('space_alloy')
+        .rarity('epic')
+    e.create('space_alloy_leggings', 'leggings')
+        .displayName('太空合金护腿')
+        .tier('space_alloy')
+        .rarity('epic')
+    e.create('space_alloy_chestplate', 'chestplate')
+        .displayName('太空合金胸甲')
+        .tier('space_alloy')
+        .rarity('epic')
+    e.create('space_alloy_pickaxe', 'pickaxe')
+        .displayName('太空合金稿')
+        .rarity('epic')
+        .tier('space_alloy')
+    e.create('space_alloy_sword', 'sword')
+        .displayName('太空合金剑')
+        .rarity('epic')
+        .tier('space_alloy')
+        .speedBaseline(-2.0)
 })
 //注册奇迹物品工具等级
-ItemEvents.toolTierRegistry(e=>{
-    e.add('miracle_ingot',tier=>{
+ItemEvents.toolTierRegistry(e => {
+    e.add('miracle_ingot', tier => {
         tier.uses = 1561;
         tier.repairIngredient = '#kubejs:miracle_item';
         tier.level = 3
@@ -55,7 +77,13 @@ ItemEvents.toolTierRegistry(e=>{
     })
 })
 //注册奇迹系列物品
-StartupEvents.registry('item', e=>{
-    e.create('miracle_pickaxe','pickaxe').displayName('奇迹稿').rarity('epic').tier('miracle_ingot')
-    e.create('miracle_shear',"shears").maxDamage(520).rarity('epic').displayName('奇迹剪')
+StartupEvents.registry('item', e => {
+    e.create('miracle_pickaxe', 'pickaxe')
+        .displayName('奇迹稿')
+        .rarity('epic')
+        .tier('miracle_ingot')
+    e.create('miracle_shear', "shears")
+        .maxDamage(520)
+        .rarity('epic')
+        .displayName('奇迹剪')
 })
