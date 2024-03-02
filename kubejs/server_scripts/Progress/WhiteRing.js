ef7f196e12694b86c8ac6254b7f4dda500ea05c7
CPearl0 <1269596929@qq.com>
2024-03-02 13:04:52 +0800

    fix

--- kubejs/server_scripts/Progress/WhiteRing.js
+++ kubejs/server_scripts/Progress/WhiteRing.js
@@ -1,3 +1,12 @@
 ServerEvents.recipes(e => {
-    
+    remove_recipes_id(e, [
+        "create:crafting/kinetics/empty_blaze_burner"
+    ])
+    e.shaped("create:empty_blaze_burner",
+        ["ABA", "BCB", "ABA"],
+        {
+            A: "#forge:rods/iron",
+            B: "#forge:plates/iron",
+            C: "#minecraft:soul_fire_base_blocks",
+        })
 })
