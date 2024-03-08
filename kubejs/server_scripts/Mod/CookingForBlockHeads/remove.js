d4ef056500a6e05d24150271b5e4901e095a093b
JasonQ1123 <nmuqindexiang@163.com>
2024-03-08 23:07:01 +0800

    懒人厨房配方更改

---
+++ kubejs/server_scripts/Mod/CookingForBlockHeads/remove.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(e=>{
+    remove_recipes_mod(e,["cookingforblockheads"])
+})
+ServerEvents.recipes(e=>{
+    e.shaped("cookingforblockheads:sink",[
+        "AAA",
+        "BCB",
+        "BBB"
+    ], {
+        A:"#forge:ingots/iron",
+        B:'#minecraft:terracotta',
+        C:'#forge:buckets/water'
+    })
+})
