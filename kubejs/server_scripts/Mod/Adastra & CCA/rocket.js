cc25650bce131e13e12a982bfb63e44aae632ac6
JasonQ1123 <nmuqindexiang@163.com>
2024-03-11 20:03:01 +0800

    fix

--- kubejs/server_scripts/Mod/Adastra & CCA/rocket.js
+++ kubejs/server_scripts/Mod/Adastra & CCA/rocket.js
@@ -10,17 +10,6 @@ BlockCrafting.addMultiblockStructure(MultiblockStructureBuilder.create("tier_1_r
     .where("b", 'alloyed:steel_scaffolding')
     .where("c", 'create_crystal_clear:andesite_clear_glass_casing')
     .where("d", 'minecraft:lightning_rod')
-    .craftingItem('#mekanism:configurators')
+    .craftingItem('create:wrench')
     .resultItem('ad_astra:tier_1_rocket')
     .build())
-BlockCrafting.addMultiblockStructure(MultiblockStructureBuilder.create("Test")
-    .pattern("ddd", "ggg")
-    .pattern("ddd", "ggg")
-    .pattern("pds", "gdg")
-    .center("s", Blocks.STONE)
-    .whereCond("d", b => b == Blocks.DIRT) // 表示 d 代表满足 b==... 的方块 b（可以用字符串）
-    .where("g", Blocks.GRASS_BLOCK) // 表示 g 代表草方块（可以用字符串）
-    .whereTag("p", "minecraft:planks") // 表示 p 代表带有 planks 标签的任意方块（可以用字符串）
-    .craftingItem('create:wrench') // 需要手持触发合成的 Item
-    .resultItem("2x minecraft:diamond") // 输出
-    .build());
