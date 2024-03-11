a6e0d7451f702537981bd7f4a60b13eb071ff61c
JasonQ1123 <nmuqindexiang@163.com>
2024-03-11 19:42:11 +0800

    tier 1 rocket recipes

---
+++ kubejs/server_scripts/Mod/Adastra & CCA/rocket.js
@@ -0,0 +1,26 @@
+BlockCrafting.addMultiblockStructure(MultiblockStructureBuilder.create("tier_1_rocket")
+    .pattern("afffa","afffa","fffff","fffff","fffff","fffff","fffff","fffff","fffff")
+    .pattern("faaaf","faaaf","faaaf","faaaf","faaaf","facaf","ffaff","fffff","fffff")
+    .pattern("faaaf","faaaf","fabaf","fabaf","fafaf","fafaf","fafaf","ffaff","ffdff")
+    .pattern("faaaf","faaaf","faeaf","faaaf","faaaf","faaaf","ffaff","fffff","fffff")
+    .pattern("afffa","afffa","fffff","fffff","fffff","fffff","fffff","fffff","fffff")
+    .center("e", Blocks.STONE)
+    .where("f", "minecraft:air")
+    .where("a", 'alloyed:steel_casing')
+    .where("b", 'alloyed:steel_scaffolding')
+    .where("c", 'create_crystal_clear:andesite_clear_glass_casing')
+    .where("d", 'minecraft:lightning_rod')
+    .craftingItem('#mekanism:configurators')
+    .resultItem('ad_astra:tier_1_rocket')
+    .build())
+BlockCrafting.addMultiblockStructure(MultiblockStructureBuilder.create("Test")
+    .pattern("ddd", "ggg")
+    .pattern("ddd", "ggg")
+    .pattern("pds", "gdg")
+    .center("s", Blocks.STONE)
+    .whereCond("d", b => b == Blocks.DIRT) // 表示 d 代表满足 b==... 的方块 b（可以用字符串）
+    .where("g", Blocks.GRASS_BLOCK) // 表示 g 代表草方块（可以用字符串）
+    .whereTag("p", "minecraft:planks") // 表示 p 代表带有 planks 标签的任意方块（可以用字符串）
+    .craftingItem('create:wrench') // 需要手持触发合成的 Item
+    .resultItem("2x minecraft:diamond") // 输出
+    .build());
