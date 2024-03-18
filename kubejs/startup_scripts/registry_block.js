0e64554f708e5c1d4cb33999ee2b8713b07deee1
JasonQ1123 <nmuqindexiang@163.com>
2024-03-18 20:06:40 +0800

    registry_block

--- kubejs/startup_scripts/registry_block.js
+++ kubejs/startup_scripts/registry_block.js
@@ -26,10 +26,12 @@ StartupEvents.registry("block", e => {
     registerBlock("miracle_block", "amethyst", 10, 10, "mineable/pickaxe", "forge:needs_netherite_tool", true)
     // 深渊合金块
     registerBlock("abyss_alloy_block", "metal", 15, 1200, "mineable/pickaxe", "forge:needs_netherite_tool", true)
-    // // 模型测试物品
+    // 模型测试物品
     registerBlock("model_test", "metal", 15, 15, "mineable/pickaxe", "minecraft:need_stone_tool", false)
-    //钢燃料储罐
+    // 钢燃料储罐
     registerBlock("steel_tank", "metal", 15, 15, "mineable/pickaxe", "minecraft:need_diamond_tool", true)
-    //钢引擎
+    // 钢引擎
     registerBlock("steel_engine", "metal", 15, 15, "mineable/pickaxe", "minecraft:need_diamond_tool", true)
+    // 一级火箭核心
+    registerBlock("rocket_core_tier_1", "metal" ,15, 15, "mineable/pickaxe", "minecraft:need_diamond_tool", true)
 })
