a99e5a2af7dbe17da78e2aec0db07bd1b477139b
JasonQ1123 <nmuqindexiang@163.com>
2024-03-22 15:47:05 +0800

    fix bronze recipes

--- kubejs/server_scripts/Mod/Alloyed/recipes.js
+++ kubejs/server_scripts/Mod/Alloyed/recipes.js
@@ -5,6 +5,6 @@ ServerEvents.recipes(e=>{
     ).id("alloyed:mixing/bronze_ingot").heated()
     e.recipes.create.mixing(
         ["3x alloyed:bronze_ingot"],
-        ["minecraft:copper_ingot", "mekanism:ingot_tin"]
+        ["3x minecraft:copper_ingot", "mekanism:ingot_tin"]
     ).id("alloyed:mixing/bronze_ingot_x3").heated()
 })
