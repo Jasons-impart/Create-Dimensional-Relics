6b0d40fc9572738ec2b3872bc187d505bf3fecf6
JasonQ1123 <nmuqindexiang@163.com>
2024-03-27 16:47:33 +0800

    fix

--- kubejs/server_scripts/Mod/TFMG/bug_fix.js
+++ kubejs/server_scripts/Mod/TFMG/bug_fix.js
@@ -8,32 +8,5 @@ ServerEvents.recipes(e =>{
     e.recipes.create.emptying([Fluid.of('createindustry:kerosene'), "minecraft:bucket"], 'createindustry:kerosene_bucket')
     e.recipes.create.emptying([Fluid.of('createindustry:diesel'), "minecraft:bucket"], 'createindustry:diesel_bucket')
     e.recipes.create.emptying([Fluid.of('ulterlands:primosoup'), "minecraft:bucket"], 'ulterlands:primosoup_bucket')
-    // 耐火黏土球与耐火黏土合成
-    e.shapeless('createindustry:fireclay', '4x createindustry:fireclay_ball')
-    e.recipes.create.crushing("4x createindustry:fireclay_ball", "createindustry:fireclay")
-    // quark石灰岩兼容石灰粉
-    e.recipes.create.crushing("createindustry:limesand", "quark:limestone")
-    e.recipes.create.milling("createindustry:limesand", "quark:limestone")
-    // 铝土矿作为副矿产生
-    e.recipes.create.crushing([
-        Item.of("create:crushed_raw_aluminum").withChance(0.1)
-    ], [
-        "create:crushed_raw_iron"
-    ])
-    e.recipes.create.crushing([
-        "create:crushed_raw_iron",
-        Item.of("create:crushed_raw_aluminum").withChance(0.3),
-        Item.of("create:experience_nugget").withChance(0.75)
-    ], [
-        "minecraft:raw_iron"
-    ])
-        .id("create:crushing/raw_iron")
-    e.recipes.create.crushing([
-        "9x create:crushed_raw_iron",
-        Item.of("create:crushed_raw_aluminum").withChance(0.3).withCount(9),
-        Item.of("create:experience_nugget").withChance(0.75).withCount(9)
-    ], [
-        "minecraft:raw_iron_block"
-    ])
-        .id("create:crushing/raw_iron_block")
+
 })
