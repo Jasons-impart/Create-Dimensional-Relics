e697d71412cd305caf37f8fff39ecbdb36a06c44
nanchuan <2020228379@qq.com>
2024-03-31 23:38:40 +0800

    配方修改

---
+++ kubejs/server_scripts/Mod/Create VI/redstone_module.js
@@ -0,0 +1,96 @@
+ServerEvents.recipes(e =>{
+    //红石构建配方修改
+     e.custom(
+      {
+        "type": "create:sequenced_assembly",
+        "ingredient": {
+          "item": "create:precision_mechanism"
+        },
+        "loops": 1,
+        "results": [
+          {
+            "item": "vintageimprovements:redstone_module"
+          }
+        ],
+        "sequence": [
+          {
+            "type": "create:deploying",
+            "ingredients": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              },
+              {
+                "item": "minecraft:redstone"
+              }
+            ],
+            "results": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ]
+          },
+          {
+            "type": "vintageimprovements:vibrating",
+            "ingredients": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ],
+            "results": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ]
+          },
+          {
+            "type": "create:deploying",
+            "ingredients": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              },
+              {
+                "tag": "forge:gems/quartz"
+              }
+            ],
+            "results": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ]
+          },
+          {
+            "type": "create:pressing",
+            "ingredients": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ],
+            "results": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ]
+          },
+          {
+            "type": "create:deploying",
+            "ingredients": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              },
+              {
+                "tag": "forge:nuggets/iron"
+              }
+            ],
+            "results": [
+              {
+                "item": "vintageimprovements:incomplete_redstone_module"
+              }
+            ]
+          }
+        ],
+        "transitionalItem": {
+          "item": "vintageimprovements:incomplete_redstone_module"
+        }
+      }
+    ).id("vintageimprovements:sequenced_assembly/redstone_module")
+  })
