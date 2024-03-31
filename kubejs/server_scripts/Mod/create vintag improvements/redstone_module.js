6c7b23e47027e1fa38f5e9d4bf98cb5fd3e33439
JasonQ1123 <nmuqindexiang@163.com>
2024-03-31 23:05:24 +0800

    shabi mixiaobao

--- kubejs/server_scripts/Mod/create vintag improvements/redstone_module.js
+++ kubejs/server_scripts/Mod/create vintag improvements/redstone_module.js
@@ -1,107 +1,96 @@
-ServerEvents.recipes(e => {
-    // 红石模块
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
-    let eirds = "create:precision_mechanism"
-    e.custom({
+ServerEvents.recipes(e =>{
+  e.custom(
+    {
+      "type": "create:sequenced_assembly",
+      "ingredient": {
+        "item": "create:precision_mechanism"
+      },
+      "loops": 1,
+      "results": [
         {
-            "type": "create:sequenced_assembly",
-            "ingredient": {
-              "tag": eirds
+          "item": "vintageimprovements:redstone_module"
+        }
+      ],
+      "sequence": [
+        {
+          "type": "create:deploying",
+          "ingredients": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
             },
-            "loops": 1,
-            "results": [
-              {
-                "item": "vintageimprovements:redstone_module"
-              }
-            ],
-            "sequence": [
-              {
-                "type": "create:deploying",
-                "ingredients": [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  },
-                  {
-                    "item": "minecraft:redstone"
-                  }
-                ],
-                "results": [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  }
-                ]
-              },
-              {
-                "type": "vintageimprovements:vibrating",
-                "":ingredients: [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  }
-                ],
-                "results": [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  }
-                ]
-              },
-              {
-                "type": "create:deploying",
-                "":ingredients [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  },
-                  {
-                    "tag": "create_new_age:copper_circuit"
-                  }
-                ],
-                "results": [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  }
-                ]
-              },
-              {
-                "type": "create:filling"
-                "ingredients": [
-                    {
-                      "item": "vintageimprovements:incomplete_redstone_module"
-
-                    },
-                    {
-                      "fluid": "createindustry:lubrication_oil",
-                      "nbt": {},
-                      "amount": 200
-                    }
-                ],
-                "results":[
-                    {
-                        "item":"vintageimprovements:incomplete_redstone_module"
-                    }
-                ]
-              }
-              {
-                "type": "create:deploying",
-                "ingredients": [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  },
-                  {
-                    "tag": "createindustry:screw"
-                  }
-                ],
-                "results": [
-                  {
-                    "item": "vintageimprovements:incomplete_redstone_module"
-                  }
-                ]
-              }
-            ],
-            "transitionalItem": {
+            {
+              "item": "minecraft:redstone"
+            }
+          ],
+          "results": [
+            {
               "item": "vintageimprovements:incomplete_redstone_module"
             }
-        )}
+          ]
+        },
+        {
+          "type": "vintageimprovements:vibrating",
+          "ingredients": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+          ],
+          "results": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+          ]
+        },
+        {
+          "type": "create:deploying",
+          "ingredients": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            },
+            {
+              "tag": "forge:gems/quartz"
+            }
+          ],
+          "results": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+          ]
+        },
+        {
+          "type": "create:pressing",
+          "ingredients": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+          ],
+          "results": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+          ]
+        },
+        {
+          "type": "create:deploying",
+          "ingredients": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            },
+            {
+              "tag": "forge:nuggets/iron"
+            }
+          ],
+          "results": [
+            {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+          ]
+        }
+      ],
+      "transitionalItem": {
+        "item": "vintageimprovements:incomplete_redstone_module"
+      }
     }
+  ).id("vintageimprovements:sequenced_assembly/redstone_module")
 })
 
