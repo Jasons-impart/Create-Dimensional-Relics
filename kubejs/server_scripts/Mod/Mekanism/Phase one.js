04b1f8bc380f1080628b25dbea3d13d7073ebaf9
mixiaobaozi <2206124398@qq.com>
2024-03-31 21:08:52 +0800

    mek小改

--- kubejs/server_scripts/Mod/Mekanism/Phase one.js
+++ kubejs/server_scripts/Mod/Mekanism/Phase one.js
@@ -36,4 +36,103 @@ ServerEvents.recipes(e => {
         .transitionalItem(perfusion)
         .loops(1)
         .id('mekanism:metallurgic_infuser')
+
+    let eirds = "create:precision_mechanism"
+    e.custom({
+        {
+            "type": "create:sequenced_assembly",
+            "ingredient": {
+              "tag": eirds
+            },
+            "loops": 1,
+            "results": [
+              {
+                "item": "vintageimprovements:redstone_module"
+              }
+            ],
+            "sequence": [
+              {
+                "type": "create:deploying",
+                "ingredients": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  },
+                  {
+                    "item": "minecraft:redstone"
+                  }
+                ],
+                "results": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  }
+                ]
+              },
+              {
+                "type": "vintageimprovements:vibrating",
+                "ingredients": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  }
+                ],
+                "results": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  }
+                ]
+              },
+              {
+                "type": "create:deploying",
+                "ingredients": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  },
+                  {
+                    "tag": "create_new_age:copper_circuit"
+                  }
+                ],
+                "results": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  }
+                ]
+              },
+              {
+                "type": "create:filling"
+                "ingredients": [
+                    {
+                      "item":
+
+                    },
+                    {
+                        
+                    }
+                ]
+
+
+
+
+
+              }
+              {
+                "type": "create:deploying",
+                "ingredients": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  },
+                  {
+                    "tag": "createindustry:screw"
+                  }
+                ],
+                "results": [
+                  {
+                    "item": "vintageimprovements:incomplete_redstone_module"
+                  }
+                ]
+              }
+            ],
+            "transitionalItem": {
+              "item": "vintageimprovements:incomplete_redstone_module"
+            }
+        }
+    }
 }) 
