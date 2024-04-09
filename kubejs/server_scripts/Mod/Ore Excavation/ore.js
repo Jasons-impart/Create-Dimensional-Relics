ce4364f352ff79fca9401bfe946c8a3849d133fc
nanchuan <2020228379@qq.com>
2024-04-09 21:43:56 +0800

    new coe

--- kubejs/server_scripts/Mod/Ore Excavation/ore.js
+++ kubejs/server_scripts/Mod/Ore Excavation/ore.js
@@ -5,7 +5,7 @@ ServerEvents.recipes(event => {
         .biomeWhitelist("minecraft:is_nether")
         .id("createoreexcavation_kjs:ancient_debris")
         .drill("createoreexcavation:netherite_drill")
-        .fluid(Fluid.of("minecraft:lava", 1000))
+        .fluid("minecraft:lava 1000")
         .stress(1024)
 })
 
@@ -13,3 +13,14 @@ ServerEvents.recipes(event => {
 ServerEvents.tags("minecraft:worldgen/biome", e => {
     e.add("kjs:has_oil", ["deeperdarker:overcast_columns", "deeperdarker:deeplands", "deeperdarker:echoing_forest"])
 })
+
+// ServerEvents.recipes(event => {
+//     // 青金石开采
+//     event.recipes.createoreexcavation.drilling("minecraft:'minecraft:lapis_ore'", "{\"text\":\"青金石\"}", 3, 3000)
+//         .veinSize(3, 8.5)
+//         .biomeWhitelist("minecraft:is_overworld")
+//         .id("createoreexcavation_kjs:'minecraft:lapis_ore'")
+//         .drill("createoreexcavation:overworld_drill")
+//         .fluid("minecraft:lava 1000")
+//         .stress(1024)
+// })
