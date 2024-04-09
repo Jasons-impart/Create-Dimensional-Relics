a3f3a3e58f13468403d91cb19d982f5b7bc47996
CPearl0 <1269596929@qq.com>
2024-04-09 22:03:14 +0800

    fix lapis ore drill

--- kubejs/server_scripts/Mod/Ore Excavation/ore.js
+++ kubejs/server_scripts/Mod/Ore Excavation/ore.js
@@ -7,20 +7,18 @@ ServerEvents.recipes(event => {
         .drill("createoreexcavation:netherite_drill")
         .fluid("minecraft:lava 1000")
         .stress(1024)
+
+    // 青金石开采
+    event.recipes.createoreexcavation.drilling("minecraft:lapis_ore", "{\"text\":\"青金石\"}", 3, 3000)
+        .veinSize(3, 8.5)
+        .biomeWhitelist("minecraft:is_overworld")
+        .id("createoreexcavation_kjs:lapis_ore")
+        .drill("#createoreexcavation:drills")
+        .fluid("minecraft:lava 1000")
+        .stress(1024)
 })
 
 // biometags添加
 ServerEvents.tags("minecraft:worldgen/biome", e => {
     e.add("kjs:has_oil", ["deeperdarker:overcast_columns", "deeperdarker:deeplands", "deeperdarker:echoing_forest"])
 })
-
-// ServerEvents.recipes(event => {
-//     // 青金石开采
-//     event.recipes.createoreexcavation.drilling("minecraft:'minecraft:lapis_ore'", "{\"text\":\"青金石\"}", 3, 3000)
-//         .veinSize(3, 8.5)
-//         .biomeWhitelist("minecraft:is_overworld")
-//         .id("createoreexcavation_kjs:'minecraft:lapis_ore'")
-//         .drill("createoreexcavation:overworld_drill")
-//         .fluid("minecraft:lava 1000")
-//         .stress(1024)
-// })
